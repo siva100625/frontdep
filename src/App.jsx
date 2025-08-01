866d33bfe0e9491b8b41934daeca64f3b65ae41e
siva100625 <sivasubramanian.v2023ece@sece.ac.ingamil.com>
2025-08-01 22:28:02 +0530

    l

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Home from './pages/Home';
 import Admin from './pages/Admin';
-import Signup from './pages/Signup';
+import Signup from './pages/SignUp';
 function App() {
   return (
     <Routes>
