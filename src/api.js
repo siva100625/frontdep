bf41de5caa5bbd219ca82edba81a101d243df033
siva100625 <sivasubramanian.v2023ece@sece.ac.ingamil.com>
2025-08-02 11:44:58 +0530

    l

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080/api', // Ensure your backend runs here
+  baseURL: 'https://springreact-5sgt.onrender.com/api', // Ensure your backend runs here
 });
 
 export default api;
